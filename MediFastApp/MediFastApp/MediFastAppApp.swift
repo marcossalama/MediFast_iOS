dfdbd740c029f3bb267bc174700f15f57faccbec
marcossalama <marcos@inventa.shop>
2025-09-04 13:59:09 -0300

    Fix: remove accidental submodule; add project files normally

---
+++ MediFastApp/MediFastApp/MediFastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediFastAppApp.swift
+//  MediFastApp
+//
+//  Created by Marcos Salama on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct MediFastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
